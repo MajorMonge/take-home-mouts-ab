6b823dc5d6918139f0da44d0512d66e0113d2800
M. Monge <luizfilipemonge@tutamail.com>
2025-05-16 16:41:28 -0300

    chore: refactor Jest configuration to use nextJest and define base config

--- apps/web/jest.config.ts
+++ apps/web/jest.config.ts
@@ -1,3 +1,22 @@
-import config from '@repo/jest-config/next';
+import type { Config } from 'jest';
+import nextJest from 'next/jest';
 
-export default config;
+export const baseConfig = {
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageProvider: 'v8',
+    moduleFileExtensions: ['js', 'ts', 'json'],
+    testEnvironment: 'jsdom',
+} as const satisfies Config;
+
+
+const createJestConfig = nextJest({
+    dir: './',
+});
+
+const config = {
+    ...baseConfig,
+    moduleFileExtensions: [...baseConfig.moduleFileExtensions, 'jsx', 'tsx'],
+} as const satisfies Config;
+
+export default createJestConfig(config);
